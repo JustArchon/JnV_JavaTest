d93a39e0e6f1e91dc8db30185554a4b9f9302884
JustArchon <puppy1900@naver.com>
2025-04-10 18:34:15 +0900

    2025-04-10

---
+++ JavaTest/JavaTest1/Num22.java
@@ -0,0 +1,13 @@
+package JavaTest1;
+
+public class Num22 {
+	
+}
+
+/*
+선택한 오답: B) How, Are, and You removed
+정답: A) Compilation fails
+컴파일 결과: 컴파일 에러
+
+이유: asList로 만들어진 리스트는 크기가 불변으로 넘어오기때문에 remove 등이 지원되지 않습니다. 그러므로 컴파일에러가 정답입니다.
+*/
---
+++ JavaTest/JavaTest1/Num31.java
@@ -0,0 +1,18 @@
+package JavaTest1;
+
+public class Num31 {
+	
+}
+
+/*
+
+Array<Cycle> myList = new ArrayList<>();
+myList.add(new MotorCycle());
+
+선택한 오답: A)모터사이클은 사이클을 구현하는 인터페이스이다. , F)모터사이클은 사이클의 상위클래스다
+정답: B) 사이클은 모터사이클을 구현하는 인터페이스이다. C) 사이클은 모터사이클의 추상화적 상위클래스다.
+
+이유: Cycle의 Array리스트에 add가 되려면 MotorCycle이 Cycle을 상속받아야합니다. 그러므로
+MotorCycle이 Cycle을 받는 B,C가 맞습니다.
+[정확히는 영어를 반대로 읽은 영어 독해 이슈]
+*/
