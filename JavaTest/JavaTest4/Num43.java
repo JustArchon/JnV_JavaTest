78d96eed049747e13fdd09529c6c44b25884005c
JustArchon <puppy1900@naver.com>
2025-04-30 16:02:17 +0900

    2025-04-30

---
+++ JavaTest/JavaTest4/Num43.java
@@ -0,0 +1,17 @@
+package JavaTest4;
+
+public class Num43 {}
+
+/*
+문제: Which two statements are valid to be written in this interface? (Choose two.)
+	 다음중 인터페이스 내에 작성할 수 있는 유효한 코드는?
+
+
+선택한 오답: D) public String methodD();
+	 	  E) public int x;
+정답: A) public abstract void methodB();
+	 D) public String methodD();
+
+이유: interface에서 변수 선언시 static final로 적용된다합니다. 적을수 있지만 반드시 초기화를 해야한다고합니다.
+
+*/
